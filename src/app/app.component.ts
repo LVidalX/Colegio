b1d1cb9bcd70c4e3ee3c6ec27cb88e97bdb9fbca
LVidalX <luisvidalpuma@gmail.com>
2024-01-25 17:18:52 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, MenuComponent, HeaderComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
